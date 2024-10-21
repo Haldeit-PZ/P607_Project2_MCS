26484d8e23486476fd82b00bf615dd8e570f2f23
Haldeit-PZ <sopherium@protonmail.com>
2024-10-21 13:30:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 setup(
     name='2D_Box',
     version='0.1.0',
-    author='Ben Maves, Haoyang Zhou',
+    author=['Ben Maves', 'Haoyang Zhou'],
     description="A Monte-Carlo Simulation of 2D Box Containing Ideal Gas Particles", 
-    author_email='hzhou51@syr.edu',
+    author_email=['sopherium@protonmail.com','']
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Haldeit-PZ/P607_Project2_MCS.git', 
@@ -32,4 +32,5 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    scripts=['codes/set_system.py'],
 )
