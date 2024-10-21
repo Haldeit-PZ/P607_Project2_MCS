35dc1a9c15de48a20651f1a8ea95015e402a1f0d
Haldeit-PZ <sopherium@protonmail.com>
2024-10-21 08:22:48 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+# Always prefer setuptools over distutils
+
+from setuptools import setup, find_packages
+
+setup(
+    name='2D_Box',
+    version='0.1.0',
+    author='Ben Maves, Haoyang Zhou',
+    description="A Monte-Carlo Simulation of 2D Box Containing Ideal Gas Particles", 
+    author_email='hzhou51@syr.edu',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/your-repo',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    entry_points={
+        'console_scripts': [
+            'my-command=scripts.my_script:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
